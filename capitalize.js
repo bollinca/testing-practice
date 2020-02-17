a1a6d69bfcec86dbcafba4f7f241922a6167b0bc
bollinca <bollinca522@gmail.com>
2020-02-17 12:54:03 -0500

    Add capitalize tests and solution

--- capitalize.js
+++ capitalize.js
@@ -1,5 +1,8 @@
-function capitalize() {
-  return 'yes';
+function capitalize(string) {
+  let stringArray = string.split('');
+  stringArray[0] = stringArray[0].toUpperCase();
+  newString = stringArray.join('')
+  return newString;
 }
 
 module.exports = capitalize
--- capitalize.test.js
+++ capitalize.test.js
@@ -1,5 +1,16 @@
 const capitalize = require('./capitalize.js');
 
-test('is connected', () => {
-  expect(capitalize()).toBe('yes')
+test('works on single lowercase words', () => {
+  expect(capitalize('hello'))
+    .toBe('Hello')
+})
+
+test('retains capitalization', () => {
+  expect(capitalize('Hello, Johnny!'))
+    .toBe('Hello, Johnny!')
+})
+
+test('does not change punctuation', () => {
+  expect(capitalize('...nope'))
+    .toBe('...nope');
 })
