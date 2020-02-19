9f4230fd8f5143f2bf84790a440415034e3257a9
bollinca <bollinca522@gmail.com>
2020-02-19 14:32:16 -0500

    Adjust final caesar tests, pass all caesar tests, refactor

--- caesar.js
+++ caesar.js
@@ -1,22 +1,32 @@
 function caesar(string, shift) {
-  const lowercaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  const uppercaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  const lowAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  const capsAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   let stringArray = string.split('');
 
-  let caesarString = stringArray.map((character) => {
-    let caesarCharacter;
-    if (lowercaseArray.includes(character)) {
-      caesarIndex = lowercaseArray.indexOf(character) + shift;
-      caesarCharacter = lowercaseArray[caesarIndex];
-    } else if (uppercaseArray.includes(character)) {
-      caesarIndex = uppercaseArray.indexOf(character) + shift;
-      caesarCharacter = uppercaseArray[caesarIndex];
+  let caesarString = stringArray.map((char) => {
+    let newChar;
+    if (lowAlphabet.includes(char)) {
+      newIndex = loopProtection(lowAlphabet.indexOf(char) + shift);
+      newChar = lowAlphabet[newIndex];
+    } else if (capsAlphabet.includes(char)) {
+      newIndex = loopProtection(capsAlphabet.indexOf(char) + shift);
+      newChar = capsAlphabet[newIndex];
     } else {
-      caesarCharacter = character;
+      newChar = char;
     }
-    return caesarCharacter;
+    return newChar;
   })
   return caesarString.join('')
+
+  function loopProtection(index) {
+    while (index >= 26) {
+      index = index - 26;
+    }
+    while (index <= -1) {
+      index = index + 26
+    }
+    return index
+  }
 }
 
 module.exports = caesar
--- caesar.test.js
+++ caesar.test.js
@@ -1,13 +1,21 @@
 const caesar = require('./caesar.js');
 
 test('shifts lowercase letters', () => {
-  expect(caesar('hello', 3)).toBe('khoor')
+  expect(caesar('hello', 3)).toBe('khoor');
 })
 
 test('works with upper and lowercase', () => {
-  expect(caesar('hElLoOO', 3)).toBe('kHoOrRR')
+  expect(caesar('hElLoOO', 3)).toBe('kHoOrRR');
 })
 
 test('retains punctuation', () => {
-  expect(caesar('hello, hello.', 3)).toBe('khoor, khoor.')
+  expect(caesar('hello, hello.', 3)).toBe('khoor, khoor.');
+})
+
+test('loops at end of alphabet (mixed case)', () => {
+  expect(caesar('Xyz', 10)).toBe('Hij');
+})
+
+test('loops with negative shift (mixed case)', () => {
+  expect(caesar('aBc', -3)).toBe('xYz');
 })
