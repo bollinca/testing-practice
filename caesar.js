53dcf2c36b8fcb464c6d97d2983da781264f1d8f
bollinca <bollinca522@gmail.com>
2020-02-19 14:01:05 -0500

    Add first two caesar tests, pass one

--- caesar.js
+++ caesar.js
@@ -1,5 +1,33 @@
-function caesar() {
-  return 'yes';
+function caesar(string, shift) {
+  const lowercaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  const uppercaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  let stringArray = string.split('');
+
+  function findNewIndexes(charArray, shift) {
+    let arrayByCaesarIndex = [];
+    charArray.forEach((character) => {
+      let caesarIndex;
+      if (lowercaseArray.includes(character)) {
+        caesarIndex = lowercaseArray.indexOf(character) + shift;
+      } else if (uppercaseArray.includes(character)) {
+        caesarIndex = uppercaseArray.indexOf(character) + shift;
+      }
+      arrayByCaesarIndex.push(caesarIndex);
+    })
+
+    return arrayByCaesarIndex;
+  }
+
+  function insertNewCharacters(indexArray) {
+    let newArray = indexArray.map((newCaesarIndex) => {
+      return lowercaseArray[newCaesarIndex];
+    });
+    return newArray.join('')
+  }
+
+  let test = findNewIndexes(stringArray, shift);
+  return insertNewCharacters(test);
+
 }
 
 module.exports = caesar
--- caesar.test.js
+++ caesar.test.js
@@ -1,5 +1,9 @@
 const caesar = require('./caesar.js');
 
-test('is connected', () => {
-  expect(caesar()).toBe('yes')
+test('shifts lowercase letters', () => {
+  expect(caesar('hello', 3)).toBe('khoor')
+})
+
+test('works with upper and lowercase', () => {
+  expect(caesar('hElLoOO', 3)).toBe('kHoOrRR')
 })
